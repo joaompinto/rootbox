f6be2d724a3a2d2d5bbf0b0a1081afad8c4bc808
João Pinto <lamego.pinto@gmail.com>
2023-04-12 20:19:54 +0200

    Added run --no-network

--- rootbox/cli/cmd_run.py
+++ rootbox/cli/cmd_run.py
@@ -7,17 +7,23 @@ import typer
 from ..download import download_image
 from ..images import parse_image_url
 from ..rootfs import prepare_rootfs
+from ..unshare import CLONE_NEWNET, unshare
 
 
 def run(
     image_name: str = typer.Argument(...),
     no_shell: bool = typer.Option(False, "--no-sh"),
+    no_net: bool = typer.Option(
+        False, "--no-network", "-N", help="Disable network in the container"
+    ),
     command: Optional[str] = typer.Argument("/bin/sh", help="Command to be run"),
     only_from_cache: bool = typer.Option(False, "--only-from-cache"),
 ):
     image = parse_image_url(image_name)
     image_fname = download_image(image, only_from_cache=only_from_cache)
     prepare_rootfs(image_fname, in_memory=True, perform_chroot=True)
+    if no_net:
+        unshare(CLONE_NEWNET)
     if no_shell:
         command = command.split()
         os.execvp(command[0], command[:])
